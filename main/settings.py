0c84e83ce7e4f8fc7869aaf6846f321d5a158bbc
Shumpei Tanaka <ShumpeiTanaka413@gmail.com>
2023-01-22 19:49:35 +0900

    a

--- main/settings.py
+++ main/settings.py
@@ -122,7 +122,7 @@ DEFAULT_CHARSET = "utf-8"
 import os
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = 'home/b1019035Study/b1019035.pythonanywhere.com/static/'
+STATIC_ROOT = '/home/b1019035Study/b1019035.pythonanywhere.com/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
