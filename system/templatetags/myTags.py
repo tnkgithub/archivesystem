aabc934c5a6c2ac57d9596bd89cc2668c6d84024
Shumpei Tanaka <ShumpeiTanaka413@gmail.com>
2023-02-23 12:21:22 +0900

    a

--- system/templatetags/myTags.py
+++ system/templatetags/myTags.py
@@ -5,6 +5,17 @@ register = template.Library()
 def get_value(dict, key):
     return dict[key]
 
+@register.simple_tag
+def for_text(value):
+    if value >= 0.5:
+        return 100
+    else :
+        return 0
+
 @register.filter
 def modulo(loopnum, value):
     return loopnum % value
+
+@register.filter
+def multiplication(value1, value2):
+    return value1 * value2
--- system/views.py
+++ system/views.py
@@ -6,7 +6,7 @@ import scipy.spatial.distance as distance
 import itertools
 import numpy as np
 # 類似度の基準値
-similarity_standard_value = 0.45
+similarity_standard_value = 0.0
 
 #%%
 # 画像によるsom結果(扱いやすくするためリストに変換)
@@ -16,7 +16,7 @@ for i in som.index:
     list_som.append(i)
 
 # 代表資料(扱いやすくするためリストに変換)
-represent_image = pd.read_csv('system/csvs/rep_image_som_result20230110_094856_3-3.csv', index_col=0)
+represent_image = pd.read_csv('system/csvs/rep_image_som_result_3-3.csv', index_col=0)
 rep_image = []
 for i in represent_image.index:
     rep_image.append(i)
@@ -32,6 +32,9 @@ links = pd.read_csv('system/csvs/imageName_link_rename_dict.csv', index_col=0).t
 
 # タイトル(管理番号あり)→画像ファイル名変換用辞書
 title_image = pd.read_csv('system/csvs/title_imageName_rename_dict.csv', index_col=0).to_dict()
+
+# 画像のベクトル表現
+image_features = pd.read_csv('system/csvs/rename_featuresTitle_pca_350_MinMax.csv', index_col=0)
 
 # タイトルのベクトル表現
 title_features = pd.read_csv('system/csvs/titleVectorsNoANDNumber.csv', index_col=0)
@@ -99,9 +102,9 @@ def SOM(request):
         id_list = request.GET["imageID"]
         id_split = id_list.split(' ')
         id = id_split[0]
-        ctx["imageID"] = id
+        ctx["spiral_dict"] = spiral_image_dict(id)
+        ctx["sorted_dict"] = part_image(id)
 
-    ctx["image_som"] = part_som(id)
     return render(request, 'system/som.html', ctx)
 
 # somとタイトル切り替え画面用
@@ -116,16 +119,20 @@ def menu(request):
 
 # タイトル画面用
 def titleSOMView(request):
+    threshold_value = 0.5
     ctx = {}
     ctx["image_title"] = image_title['col2']
     ctx["links"] = links['col2']
+
 
     ''' urlから表示したい画像のidタグを取得 '''
     if "imageID" in request.GET:
         id_list = request.GET["imageID"]
         id_split = id_list.split(' ')
         id = id_split[0]
-        ctx["title_list"] = spiral_list(id)
+        ctx["spiral_dict"] = spiral_title_dict(id)
+        ctx["sorted_dict"] = part_title(id)
+
     return render(request, 'system/title-som.html', ctx)
 
 #%%
@@ -137,13 +144,14 @@ def sort_by_similarity(dict):
     return sorted_dict
 
 ''' タイトルの類似度を計算し、ソートして返す '''
-def sortForSimilarity(image):
+def sortForSimilarity(id):
     # 画像ファイル名→タイトル(管理番号あり)に変換用
     title_dict = image_title_num['col2']
     # タイトルを探す
-    title = title_dict[image]
+    title = title_dict[id]
     # ソート後の辞書
     sorted_dict = {}
+    sorted_dict[id] = 1.0
     # タイトル(管理番号あり)→画像ファイル名に変換用
     dict_title_image = title_image['col2']
     ''' 類似度を計算し、辞書を作成 '''
@@ -157,17 +165,24 @@ def sortForSimilarity(image):
             if sim >= similarity_standard_value:
                 # 辞書に追加
                 sorted_dict[image_name] = sim
-
     sorted_dict = sort_by_similarity(sorted_dict)
-    sorted_list = [image]
+    '''
     for key in sorted_dict.keys():
         sorted_list.append(key)
-    return sorted_list
+    '''
+    return sorted_dict
 
 #%%
-def spiral_list(image):
+def spiral_title_dict(id):
 
-    sorted_list = sortForSimilarity(image)
+    sorted_dict = sortForSimilarity(id)
+
+    sorted_key_list = []
+    sorted_value_list = []
+    for key, value in sorted_dict.items():
+        sorted_key_list.append(key)
+        sorted_value_list.append(value)
+
 
     LOOP = 5
     WIDTH = (2*LOOP) + 1
@@ -207,18 +222,122 @@ def spiral_list(image):
     toList = []
     toList = sum(spiral, [])
     image_list = ['-1'] * len(toList)
-    # 螺旋を表示する
-    for i in range(len(sorted_list)):
+    sim_list = ['-1'] * len(toList)
+    # 螺旋リストに代入
+    for i in range(len(sorted_key_list)):
         for j in range(len(toList)):
             if i == toList[j]:
-                image_list[j] = sorted_list[i]
-                print(i, image_list[j])
+                image_list[j] = sorted_key_list[i]
+                sim_list[j] = sorted_value_list[i]
 
-    return(image_list)
+    spiral_dict = dict(zip(image_list, sim_list))
+    return spiral_dict
 
+
+def part_title(id):
+    sorted_dict = sortForSimilarity(id)
+    count = 0
+    part_dict = {}
+    for key, value in sorted_dict.items():
+        part_dict[key] = value
+        count += 1
+        if count == 100:
+            return part_dict
+
+
+
+''' タイトルの類似度を計算し、ソートして返す '''
+def sort_image_Similarity(id):
+    # ソート後の辞書
+    sorted_dict = {}
+    sorted_dict[id] = 1.0
+    ''' 類似度を計算し、辞書を作成 '''
+    for i in image_features.index:
+        if i != id:
+            # コサイン類似度を計算
+            sim = 1 - distance.cosine(image_features.loc[id].to_list(), image_features.loc[i].to_list())
+            ''' 類似度が基準値以上のみにする '''
+            if sim >= similarity_standard_value:
+                # 辞書に追加
+                sorted_dict[i] = sim
+    sorted_dict = sort_by_similarity(sorted_dict)
+    '''
+    for key in sorted_dict.keys():
+        sorted_list.append(key)
+    '''
+    return sorted_dict
+
+def spiral_image_dict(id):
+
+    sorted_dict = sort_image_Similarity(id)
+
+    sorted_key_list = []
+    sorted_value_list = []
+    for key, value in sorted_dict.items():
+        sorted_key_list.append(key)
+        sorted_value_list.append(value)
+
+
+    LOOP = 5
+    WIDTH = (2*LOOP) + 1
+
+    E = (1, 0)
+    N = (0, -1)
+    W = (-1, 0)
+    S = (0, 1)
+    DIRECTION = itertools.cycle((E, S, W, N))
+
+    x = LOOP
+    y = LOOP
+    step = 1    # 進んだ距離
+    corner = 1  # まがり角の位置
+
+    # 二次元リストを初期化
+    spiral = []
+    for i in range(WIDTH):
+        spiral.append([0 for j in range(WIDTH)])
+
+    for i in range(WIDTH * WIDTH):
+        # まがり角に到達したら方向転換
+        if step >= corner:
+            step = 1
+            direction = next(DIRECTION)
+            dx, dy = direction
+
+            # X方向に進むとき、まがり角が遠くなる
+            if direction == E or direction == W:
+                corner += 1
+
+        spiral[y][x] = i
+        step += 1
+        x += dx
+        y += dy
+
+    toList = []
+    toList = sum(spiral, [])
+    image_list = ['-1'] * len(toList)
+    sim_list = ['-1'] * len(toList)
+    # 螺旋リストに代入
+    for i in range(len(sorted_key_list)):
+        for j in range(len(toList)):
+            if i == toList[j]:
+                image_list[j] = sorted_key_list[i]
+                sim_list[j] = sorted_value_list[i]
+
+    spiral_dict = dict(zip(image_list, sim_list))
+    return spiral_dict
 
 #%%
-def part_som(id):
+def part_image(id):
+    sorted_dict = sort_image_Similarity(id)
+    count = 0
+    part_dict = {}
+    for key, value in sorted_dict.items():
+        part_dict[key] = value
+        count += 1
+        if count == 100:
+            return part_dict
+    '''
     np_array = np.empty_like(list_som)
     for i in range(len(list_som)):
         np_array[i] = list_som[i]
@@ -248,3 +367,4 @@ def part_som(id):
     part_list = []
     part_list = sum(part_list_2dem, [])
     return part_list
+    '''
